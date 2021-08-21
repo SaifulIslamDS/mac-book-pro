07145d20a127c55eb8b3daaf81f36f667a7a3d66
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-08-21 08:07:07 +0600

    Update all functionalities

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 // Product name= Macbook Pro, Default price = $1299
 const macbookPrice = document.getElementById("macbook-price");
+
 // Extra memory part buttons 
 const memory8 = document.getElementById("memory-8");
 const memory16 = document.getElementById("memory-16");
 const extraMemoryPrice = document.getElementById("extra-memory-price");
+
 // Extra storage part buttons
 const storage256 = document.getElementById("storage-256");
 const storage512 = document.getElementById("storage-512");
 const storage1tb = document.getElementById("storage-1tb");
 const extraStoragePrice = document.getElementById("extra-storage-price");
+
 // Delivery options part buttons
 const freeDelivery = document.getElementById("free-delivery");
 const expressDelivery = document.getElementById("express-delivery");
@@ -16,7 +19,8 @@ const deliveryCharge = document.getElementById("delivery-charge");
 
 // Cart Total
 const cartTotal = document.getElementById("cart-total");
-
+// Grand total 
+const grandTotal = document.getElementById("grand-total") ;
 // Function: Update cart total
 function updateCartTotal() {
     const productPrice =  parseFloat(macbookPrice.innerText);
@@ -24,6 +28,7 @@ function updateCartTotal() {
     const storagePrice = parseFloat(extraStoragePrice.innerText);
     const dliveryCost = parseFloat(deliveryCharge.innerText);
     cartTotal.innerText = productPrice + memoryPrice + storagePrice + dliveryCost;
+    grandTotal.innerText = cartTotal.innerText;
 }
 
 // Event listeners: Memory
@@ -58,10 +63,29 @@ freeDelivery.addEventListener("click", function(){
 expressDelivery.addEventListener("click", function(){
     deliveryCharge.innerText = 20;
     updateCartTotal();
-    console.log(deliveryCharge.innerText);
 });
 
-// Coup
 
 
+// Coupon code application
+const promoCodeBtn = document.getElementById("promo-code-btn");
+const promoCodeValue = document.getElementById("promo-code-value");
 
+/* 
+>> Coupon discout calculation function:
+>> Function name: applyCoupon()
+>> Coupon code: stevekaku
+>> If applied, 20% price from grand total will be reduced  
+ */
+function applyCoupon(){
+    if (promoCodeValue.value == "stevekaku") {
+        grandTotal.innerText = cartTotal.innerText - (cartTotal.innerText * 20)/100;
+    } else {
+        const errorMsg = document.getElementById("error-msg");
+        errorMsg.style.display = "block";
+    }
+}
+// Adding event listener for promocode apply button with the applyCoupon() function 
+promoCodeBtn.addEventListener("click", function(){
+    applyCoupon();
+});
